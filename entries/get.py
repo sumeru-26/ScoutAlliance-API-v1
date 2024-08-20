043af1ec5c5bc2f67fe45fe8432135ad6ee866c5
sumeru-26 <155037091+sumeru-26@users.noreply.github.com>
2024-08-20 15:03:54 -0700

    added querying entries

--- entries/get.py
+++ entries/get.py
@@ -1,17 +1,41 @@
-import json
+# import os
+# import sys
+# from pprint import pprint  # noqa: F401
 
-from db import entries_db
+# current = os.path.dirname(os.path.realpath(__file__))
+# parent = os.path.dirname(current)
+# sys.path.append(parent)
+
+# comment out above when uploading, only necessary for local development
+
+import json  # noqa: E402
+
+from db import entries_db  # noqa: E402
 
 def lambda_handler(event, context):
     
     team = str(event['requestContext']['authorizer']['lambda']['team'])
-    
-    cursor = entries_db[team].find({}, {'_id': 0})
+
+    query = format_queries(event['queryStringParameters']) if 'queryStringParameters' in event.keys() else {}
+
+    cursor = entries_db[team].find(query, {'_id': 0})
     data = []
     for x in cursor:
         data.append(x)
     
     return {
         'statusCode': 200,
+        'headers': {
+            'content-type':'application/json'
+        },
         'body': json.dumps(data)
     }
+
+def format_queries(raw_queries):
+    formatted_queries = {}
+    for field, val in raw_queries.items():
+        if val.isdigit():
+            val = int(val)
+        formatted_queries[field] = val
+    print(formatted_queries)
+    return formatted_queries
